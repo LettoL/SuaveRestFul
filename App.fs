f23106394fb6a9c78c14572d79d9d534c1d315a7
Denis <letto-l@yandex.ru>
2019-10-10 23:00:57 +0300

    started

---
+++ App.fs
@@ -0,0 +1,12 @@
+﻿open Suave.Web
+open Suave.Successful
+open SuaveRestApi.Db
+open SuaveRestApi.Rest
+
+[<EntryPoint>]
+let main argv =
+    let personWebPart = rest "people" {
+      GetAll = Db.getPeople
+    }
+    startWebServer defaultConfig personWebPart
+    0
---
+++ RestFul.fs
@@ -0,0 +1,27 @@
+﻿namespace SuaveRestApi.Rest
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+open Suave
+open Suave.Successful
+open Suave.Operators
+open Suave.Filters
+
+[<AutoOpen>]
+module RestFul =
+  type RestResource<'a> = {
+    GetAll : unit -> 'a seq
+  }
+  
+  let JSON v =
+    let settings = new JsonSerializerSettings()
+    settings.ContractResolver <-
+      new CamelCasePropertyNamesContractResolver()
+      
+    JsonConvert.SerializeObject(v, settings)
+    |> OK
+    >=> Writers.setMimeType "application/json; charset=utf-8"
+  
+  let rest resourceName resource =
+    let resourcePath = "/" + resourceName
+    let getAll = warbler (fun _ -> resource.GetAll () |> JSON)
+    path resourcePath >=> GET >=> getAll
